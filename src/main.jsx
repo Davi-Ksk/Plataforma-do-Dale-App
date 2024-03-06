28f889d89a411cc2d38b8c2706d3daee18cf910f
davi <davikraieski@gmail.com>
2024-03-06 11:15:26 -0300

    index setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import GlobalStyles from './Styles/global';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './Styles/theme'
+
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+          <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
 )
+
