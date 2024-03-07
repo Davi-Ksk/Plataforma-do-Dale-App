245706e701512b4941104b1d8f74ab6e3c3a6fd2
davi <davikraieski@gmail.com>
2024-03-07 14:18:37 -0300

    add Home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "styles.js";
+import { Nav } from "components/Nav";
+
+export function Home() {
+  return (
+    <Container>
+
+      <Content>
+
+      </Content>
+    </Container>
+  )
+}
