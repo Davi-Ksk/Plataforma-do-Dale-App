8d69489d1306a627a0e9d6d11f75a376a18b1610
davi <davikraieski@gmail.com>
2024-03-07 14:20:45 -0300

    added react icons

---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { PiFunnelSimple, PiList } from "react-icons/pi";
+
+export function Nav() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
