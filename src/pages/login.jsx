b1642ed9283d79be10067eb67ba6f5d76578cc78
Davs <davidsilvateixeira5@hotmail.com>
2024-03-16 04:11:57 -0300

    pagina de login atualizada

---
+++ src/pages/login.jsx
@@ -0,0 +1,30 @@
+import './style.css';
+
+function App() {
+  return (
+    <main class="maincontainer">
+      <section class="container">
+        <div class="content">
+          <h1>Conecta <br/>Talentos</h1>
+          <p class="subtitle">A plataforma de talentos top</p>
+        </div>
+
+        <form action="" class="formData">
+          <div class="inputBox">
+            <input type="login" placeholder='Digite seu login'/>
+          </div>
+
+          <div class="inputBox">
+            <input type="login"/>
+          </div>
+          
+          <div class="inputButton">
+            <button>Entrar</button>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default App;
